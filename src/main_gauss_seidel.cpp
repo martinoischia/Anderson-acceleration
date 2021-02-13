19d277c6f7ddaaedd0c9b972aa8b6ec67bf413fa
Martino Ischia <ischiamartino@gmail.comq>
2021-02-13 11:52:17 +0100

    rewritten the last code, also fixed an error on matrix coefficient, but i will check now

--- src/main_gauss_seidel.cpp
+++ src/main_gauss_seidel.cpp
@@ -1,48 +1,13 @@
 //! @file main_gauss_seidel.cpp
 //! @brief A simple discretization of a differential problem
 //!
-//! Solves 1D heat equation on a bar
+//! Solves 1D heat equation on a bar through Gauss-Seidel fixed-point iterations.
+//! Implements also an alternating Anderson accelerator.
+#include "gauss_seidel.cpp"
 #include "FixedPointIterator.hpp"
 #include "GetPot"
 #include "Timing.hpp"
 #include <iomanip>
-
-//! @brief Gauss-Seidel solver
-//!
-//! It is taken from the HeatExchange directory in the repository PACS of Luca Formaggia,
-//! have a look there to understand what is going on.
-class LinearSolver
-{
-	public:
-	using Vector = FixedPoint::Traits::Vector;
-	//constructor
-	LinearSolver(const int size): M(size){};
-	//call operator
-	Vector operator ()(const Vector & x)
-	{
-		Vector y(M);
-		y[0] = ((To-Te)/Te + x[1])/( 2.+h*h*act );
-		for(int m=1; m < M-1; ++m)
-		{  
-			y[m]  = (y[m-1]+x[m+1])/( 2.+h*h*act );
-		}      
-		y[M-1] = y[M-2];
-		return y;
-	}
-	static constexpr double L= 40.;  // Bar length
-	static constexpr double a1=4.; // First longitudinal dimension
-	static constexpr double a2=50; //  Second longitudinal dimension
-	static constexpr double To=46.; // Dirichlet condition
-	static constexpr double Te=20.; // External temperature (Centigrades)
-	static constexpr double k=0.164;  // Thermal conductivity
-	static constexpr double hc=200.0e-6; // Convection coefficient
-	static constexpr double act=2.*(a1+a2)*hc*L*L/(k*a1*a2);
-	const int M; // Number of grid elements
-	//! Precomputed coefficient for adimensional form of equation
-	// mesh size
-	const double h=1./M;
-
-};
 
 int main()
 {
@@ -50,10 +15,9 @@ int main()
 	std::cout << std::fixed;
 	using namespace FixedPoint;
 	using Vector = Traits::Vector;
-	using Matrix = Traits::Matrix;
 	GetPot get_problem_data ("data.input");
 	int dimension = get_problem_data ("FPI_3_param/M", 7);
-	LinearSolver solver(dimension);
+	GaussSeidelIterator solver(dimension, Vector::Zero(dimension), 0.);
 	
 	// Initial vector: a linear variation of T
 	Vector theta(dimension);
@@ -71,7 +35,6 @@ int main()
 	FPI_3.getOptions().memory = get_problem_data ("FPI_3_param/memory", 100);
 	FPI_3.getOptions().tolerance = get_problem_data ("FPI_3_param/tolerance", 0.01);
 	FPI_3.getOptions().printMemory = get_problem_data ("FPI_3_param/printMemory", 100);
-	
 	//Solving
 	std::cout<<"\n*** WITH BASIC METHOD:\n\n";
 	Timing t;
@@ -80,12 +43,12 @@ int main()
 	FPI_3.printResidualHistory();
 	std::cout<<"\n L^infinity norm of the error: " << ((FPI_3.getHistory().back()+Vector::Ones(dimension))*solver.Te-true_sol).maxCoeff()/26. <<std::endl;
 	FPI_3.printResult();
+	
 	// Now with the Anderson Accelerator
 	FPI_3.reset();
 	double mixingParameter = get_problem_data ("FPI_3_param/mixingParameter", 1.);
 	std::size_t memory = get_problem_data ("FPI_3_param/AndersonMemory", 5);
 	FPI_3.setIterator( std::make_unique <AndersonAccelerator> (std::move (FPI_3.getIterator().getIterationFunction()), dimension, mixingParameter, memory) );
-	
 	//Solving
 	std::cout<<"\n*** WITH ANDERSON ACCELERATION:\n\n";
 	t.reset();
@@ -94,12 +57,15 @@ int main()
 	FPI_3.printResidualHistory();
 	std::cout<<"\n L^infinity norm of the error: " << ((FPI_3.getHistory().back()+Vector::Ones(dimension))*solver.Te-true_sol).maxCoeff()/26. <<std::endl;
 	FPI_3.printResult();
+	// Just for my postprocessing
 	if ( good ) std::cout << "true" <<std::endl;
+	
+	
 	// Now alternating Gauss-Seidel iterations to Anderson iterations
 	// I swap unique pointers to Iterator objects
 	FPI_3.reset();
 	FPI_3.setIterator( std::make_unique <Iterator> (std::move (FPI_3.getIterator().getIterationFunction()), dimension) );
-	LinearSolver solver_copy(dimension);
+	GaussSeidelIterator solver_copy(dimension, Vector::Zero(dimension), 0.);
 	std::unique_ptr<Iterator> IteratorToSwap = std::make_unique <AndersonAccelerator> (std::move (solver_copy), dimension, mixingParameter, memory);
 	double alternatingParameter = get_problem_data ("FPI_3_param/alternatingParameter", 10);
 	//Solving
--- src/main_nonlinear.cpp
+++ src/main_nonlinear.cpp
@@ -6,60 +6,30 @@
 #include "GetPot"
 #include "Timing.hpp"
 #include <stdexcept>
+#include "gauss_seidel.cpp"
 // #include <iomanip>
 
-//! @brief Gauss-Seidel solver
-//!
-//! It is taken from the HeatExchange directory in the repository PACS of Luca Formaggia,
-//! have a look there to understand what is going on.
 namespace FixedPoint
-{
-	
-	class LinearSolver2
+{	
+	//! It is basically the same alternating solver that was used in main_gauss_seidel.cpp
+	class AlternatingSolver: public Traits
 	{
+		private: 
+		//! Newton solver or not
+		bool Newton;
+		
 		public:
-		using Vector = Traits::Vector;
-		//constructor
-		LinearSolver2(const int size, const Vector & v): M(size), x(v){};
-		//call operator
-		Vector operator ()(const Vector & z) const
-		{
-			Vector y(M);
-			y[0] = ((To-Te)/Te + z[1])/( 2.+h*h* ( act+ sigma *x[0]*x[0]*x[0] ));
-			for(int m=1; m < M-1; ++m)
-			{  
-				y[m]  = (y[m-1]+z[m+1])/(2.+h*h* ( act + sigma *x[m]*x[m]*x[m] ));
-			}      
-			y[M-1] = 1./( 1.+ sigma * h*h*x[M-1]*x[M-1]*x[M-1] ) * y[M-2];
-			return y;
-		}
-		static constexpr double L= 40.;  // Bar length
-		static constexpr double a1=4.; // First longitudinal dimension
-		static constexpr double a2=50; //  Second longitudinal dimension
-		static constexpr double To=46.; // Dirichlet condition
-		static constexpr double Te=20.; // External temperature (Centigrades)
-		static constexpr double k=0.164;  // Thermal conductivity
-		static constexpr double hc=200.0e-6; // Convection coefficient
-		static constexpr double act=2.*(a1+a2)*hc*L*L/(k*a1*a2);
-		static constexpr double sigma = 1.; //Radiation term
-		const int M; // Number of grid elements
-		//! Precomputed coefficient for adimensional form of equation
-		// mesh size
-		const double h=1./M;
-		const Vector & x;
-	};
-	
-	
-	class FixedPointLinearSolver
-	{
-		public:
-		using Vector = Traits::Vector;
+		//! Constructor
+		AlternatingSolver ( bool N = false ) : Newton (N) {};
+		//! Call operator
 		Vector operator ()(Vector const & x) const {
-			using namespace FixedPoint;
 			GetPot get_problem_data ("data.input");
 			int dimension = get_problem_data ("FPI_3_param/M", 7);
-			LinearSolver2 solver(dimension, x);
-			
+			GaussSeidelIterator solver(dimension, x, 1.);
+			if (Newton) {
+				solver.rhs = solver.f() ;
+				solver.sigma *= 4.;
+			}
 			// Initial vector: a linear variation of T
 			Vector theta(dimension);
 			for(int m=0; m<dimension; ++m)
@@ -74,7 +44,11 @@ namespace FixedPoint
 			std::size_t memory = get_problem_data ("FPI_3_param/AndersonMemory", 5);
 			
 			// Alternating Gauss-Seidel iterations to Anderson iterations
-			LinearSolver2 solver_copy(dimension, x);
+			GaussSeidelIterator solver_copy(dimension, x, 1.);
+			if (Newton) {
+				solver_copy.rhs = solver_copy.f() ;
+				solver_copy.sigma *= 4.; 
+			}
 			std::unique_ptr<Iterator> IteratorToSwap = std::make_unique <AndersonAccelerator> (std::move (solver_copy), dimension, mixingParameter, memory);
 			double alternatingParameter = get_problem_data ("FPI_3_param/alternatingParameter", 10);
 			//Solving
@@ -100,112 +74,7 @@ namespace FixedPoint
 			}
 			if (!converged) throw std::runtime_error( "the linear step did non converge" ) ;
 			// std::cout <<"inner iterations " << Iterations << std::endl;
-			return FPI_3.getHistory().back();
-		}
-		
-	};
-	
-
-	
-	
-	
-	class LinearSolver3
-	{
-		public:
-		using Vector = Traits::Vector;
-		//constructor
-		LinearSolver3(const int size, const Vector & v): M(size), x(v){};
-		//call operator
-		Vector operator ()(const Vector & z) const
-		{
-			Vector rhs = f();
-			Vector y(M);
-			y[0] = ( rhs[0] + z[1])/( 2.+h*h* ( act+ 4*sigma *x[0]*x[0]*x[0] ));
-			for(int m=1; m < M-1; ++m)
-			{  
-				y[m]  = (y[m-1]+z[m+1]+rhs[m])/(2.+h*h* ( act + 4*sigma *x[m]*x[m]*x[m] ));
-			}      
-			y[M-1] = 1./( 1.+ sigma * x[M-1]*x[M-1]*x[M-1] ) * (y[M-2]+rhs[M-1]);
-			return y;
-		}
-		static constexpr double L= 40.;  // Bar length
-		static constexpr double a1=4.; // First longitudinal dimension
-		static constexpr double a2=50; //  Second longitudinal dimension
-		static constexpr double To=46.; // Dirichlet condition
-		static constexpr double Te=20.; // External temperature (Centigrades)
-		static constexpr double k=0.164;  // Thermal conductivity
-		static constexpr double hc=200.0e-6; // Convection coefficient
-		static constexpr double act=2.*(a1+a2)*hc*L*L/(k*a1*a2);
-		static constexpr double sigma = 1.; //Radiation term
-		const int M; // Number of grid elements
-		//! Precomputed coefficient for adimensional form of equation
-		// mesh size
-		const double h=1./M;
-		const Vector & x;
-		
-		private:
-		Vector f () const{
-			Vector y(M);
-			y[0]= (2.+h*h* (act + sigma* x[0]*x[0]*x[0]))*x[0]-x[1]-(To-Te)/Te;
-			for(int m=1; m < M-1; ++m){
-				y[m]= (2.+h*h*(act + sigma* x[m]*x[m]*x[m]))*x[m]-x[m-1] - x[m+1];
-			}
-			y[M-1] = (1.+ sigma*h*h* x[M-1]*x[M-1]*x[M-1])*x[M-1]-x[M-2];
-			return y;
-		}
-	};	
-	
-		class NewtonFixedPointLinearSolver
-	{
-		public:
-		using Vector = Traits::Vector;
-		Vector operator ()(Vector const & x) const {
-			using namespace FixedPoint;
-			GetPot get_problem_data ("data.input");
-			int dimension = get_problem_data ("FPI_3_param/M", 7);
-			LinearSolver3 solver(dimension, x);
-			
-			// Initial vector: a linear variation of T
-			Vector theta(dimension);
-			for(int m=0; m<dimension; ++m)
-			theta[m]=(1.-(m+1)*solver.h)*(solver.To-solver.Te)/solver.Te;
-			
-			//The iterator object
-			FixedPointIterator FPI_3;
-			FPI_3.setIterator( std::make_unique <Iterator> (std::move(solver), dimension) );
-			FPI_3.getOptions().memory = get_problem_data ("FPI_3_param/memory", 100);
-			FPI_3.getOptions().tolerance = get_problem_data ("FPI_3_param/tolerance", 0.01);
-			double mixingParameter = get_problem_data ("FPI_3_param/mixingParameter", 1.);
-			std::size_t memory = get_problem_data ("FPI_3_param/AndersonMemory", 5);
-			
-			// Alternating Gauss-Seidel iterations to Anderson iterations
-			LinearSolver3 solver_copy(dimension, x);
-			std::unique_ptr<Iterator> IteratorToSwap = std::make_unique <AndersonAccelerator> (std::move (solver_copy), dimension, mixingParameter, memory);
-			double alternatingParameter = get_problem_data ("FPI_3_param/alternatingParameter", 10);
-			//Solving
-			auto & stop = FPI_3.getOptions().maxIter;
-			auto maxIter = get_problem_data ("FPI_3_param/maxIter", 100);
-			bool converged = false;
-			unsigned int Iterations = 0;
-			while( !converged and Iterations < maxIter ){
-				stop = alternatingParameter;
-				FPI_3.getIteration() = 0;
-				converged = FPI_3.compute( theta );
-				Iterations += FPI_3.getIteration();
-				if( !converged ) 
-				{
-					FPI_3.getIterator_p().swap( IteratorToSwap );
-					stop = 1;
-					FPI_3.getIteration() = 0;
-					FPI_3.getIterator().SetUp( FPI_3.getHistory() );
-					converged = FPI_3.compute();
-					++Iterations;
-					FPI_3.getIterator_p().swap( IteratorToSwap );
-				}
-			}
-			if (!converged) throw std::runtime_error( "the linear step did non converge" ) ;
-			// std::cout <<"inner iterations " << Iterations << std::endl;
-			return (x - FPI_3.getHistory().back());
+			return (Newton? (x - FPI_3.getHistory().back()) : FPI_3.getHistory().back());
 		}
 		
 	};
@@ -216,24 +85,20 @@ int main()
 	// std::cout << std::fixed;
 	using namespace FixedPoint;
 	using Vector = Traits::Vector;
-	using Matrix = Traits::Matrix;
 	GetPot get_problem_data ("data.input");
 	int dimension = get_problem_data ("FPI_3_param/M", 7);
 	
-	// Initial vector: a linear variation of T
+	// Initial vector: constant T
 	Vector theta(dimension);
-	{
-		LinearSolver2 solver (dimension, Vector{} );
-		for(int m=0; m<dimension; ++m)
-		theta[m]=(solver.To-solver.Te)/solver.Te;
-	}
+	for(int m=0; m<dimension; ++m)
+	theta[m]=(GaussSeidelIterator::To-GaussSeidelIterator::Te)/GaussSeidelIterator::Te;
 	//The iterator object
 	FixedPointIterator FPI_3;
-	FPI_3.setIterator( std::make_unique <Iterator> ( FixedPointLinearSolver{}, dimension) );
-	FPI_3.getOptions().maxIter = get_problem_data ("FPI_3_param/maxIter", 100);
-	FPI_3.getOptions().memory = get_problem_data ("FPI_3_param/memory", 100);
-	FPI_3.getOptions().tolerance = get_problem_data ("FPI_3_param/tolerance", 0.01);
-	FPI_3.getOptions().printMemory = get_problem_data ("FPI_3_param/printMemory", 100);
+	FPI_3.setIterator( std::make_unique <Iterator> ( AlternatingSolver(), dimension) );
+	FPI_3.getOptions().maxIter = get_problem_data ("newton_param/maxIter", 100);
+	FPI_3.getOptions().memory = get_problem_data ("newton_param/memory", 100);
+	FPI_3.getOptions().tolerance = get_problem_data ("newton_param/tolerance", 0.01);
+	FPI_3.getOptions().printMemory = get_problem_data ("newton_param/printMemory", 100);
 	
 	//Solving
 	std::cout<<"\n*** WITH BASIC METHOD:\n\n";
@@ -247,8 +112,7 @@ int main()
 	
 	// Now with Newton method
 	FPI_3.reset();
-	FPI_3.setIterator( std::make_unique <Iterator> (NewtonFixedPointLinearSolver{}, dimension ));
-	
+	FPI_3.setIterator( std::make_unique <Iterator> ( AlternatingSolver (true), dimension ) );
 	//Solving
 	std::cout<<"\n*** WITH NEWTON METHOD:\n\n";
 	t.reset();
@@ -257,12 +121,12 @@ int main()
 	FPI_3.printResidualHistory();
 	FPI_3.printResult();
 	Vector v2 = FPI_3.getHistory().back();
+	
 	// Now with the Anderson Accelerator
 	FPI_3.reset();
-	double mixingParameter = get_problem_data ("FPI_3_param/mixingParameter", 1.);
-	std::size_t memory = get_problem_data ("FPI_3_param/AndersonMemory", 5);
-	FPI_3.setIterator( std::make_unique <AndersonAccelerator> ( FixedPointLinearSolver{}, dimension, mixingParameter, memory) );
-	
+	double mixingParameter = get_problem_data ("newton_param/mixingParameter", 1.);
+	std::size_t memory = get_problem_data ("newton_param/AndersonMemory", 5);
+	FPI_3.setIterator( std::make_unique <AndersonAccelerator> ( AlternatingSolver(), dimension, mixingParameter, memory) );
 	//Solving
 	std::cout<<"\n*** WITH ANDERSON ACCELERATION:\n\n";
 	t.reset();
@@ -271,17 +135,16 @@ int main()
 	FPI_3.printResidualHistory();
 	FPI_3.printResult();
 	Vector v3 = FPI_3.getHistory().back();
-
-	// Now alternating Gauss-Seidel iterations to Anderson iterations
-	// I swap unique pointers to Iterator objects
+	
+	// Now with alternating method
 	FPI_3.reset();
-	FPI_3.setIterator( std::make_unique <Iterator> (std::move (FPI_3.getIterator().getIterationFunction()), dimension) );
-	std::unique_ptr<Iterator> IteratorToSwap = std::make_unique <AndersonAccelerator> (FixedPointLinearSolver{}, dimension, mixingParameter, memory);
-	double alternatingParameter = get_problem_data ("FPI_3_param/alternatingParameter", 10);
+	FPI_3.setIterator( std::make_unique <Iterator> ( AlternatingSolver(), dimension) );
+	std::unique_ptr<Iterator> IteratorToSwap = std::make_unique <AndersonAccelerator> ( AlternatingSolver(), dimension, mixingParameter, memory);
+	double alternatingParameter = get_problem_data ("newton_param/alternatingParameter", 10);
 	//Solving
 	std::cout<<"\n*** WITH ALTERNATING ANDERSON METHOD:\n\n";
 	auto & stop = FPI_3.getOptions().maxIter;
-	auto maxIter = get_problem_data ("FPI_3_param/maxIter", 100);
+	auto maxIter = get_problem_data ("newton_param/maxIter", 100);
 	bool converged = false;
 	unsigned int Iterations = 0;
 	t.reset();
